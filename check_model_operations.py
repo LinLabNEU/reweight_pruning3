ec630985c5916fa07094fbfa4d98134f7a9641e2
LinLabNEU <zhaopuglory@gmail.com>
2019-10-11 13:56:40 -0400

    Update check_model_operations.py

--- check_model_operations.py
+++ check_model_operations.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 import matplotlib.pyplot as plt
 from testers import *
-from vgg import VGG
 from collections import OrderedDict
 import numpy as np
 from mobilenetv2 import MobileNetV2
@@ -17,7 +16,7 @@ device = torch.device("cuda" if use_cuda else "cpu")
 
 
 test_loader = torch.utils.data.DataLoader(
-    datasets.CIFAR100('./data.cifar100', train=False, transform=transforms.Compose([
+    datasets.CIFAR100('./data.cifar100', train=False, download=True, transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])),
